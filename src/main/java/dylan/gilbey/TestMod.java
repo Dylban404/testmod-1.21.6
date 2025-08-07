05f60b1aca9adc2936162909668655645960daa4
Dylan <hatoodeer116@gmail.com>
2025-08-07 21:01:28 +0100

    Added comment!

--- src/main/java/dylan/gilbey/TestMod.java
+++ src/main/java/dylan/gilbey/TestMod.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ModInitializer;
 
+// comment
 public class TestMod implements ModInitializer {
 	public static final String MOD_ID = "test-mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
