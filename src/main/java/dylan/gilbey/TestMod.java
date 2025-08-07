14fa0786aaf005b72d97581383b93ad676803492
Dylan <hatoodeer116@gmail.com>
2025-08-07 20:59:58 +0100

    Initial commit

---
+++ src/main/java/dylan/gilbey/TestMod.java
@@ -0,0 +1,16 @@
+package dylan.gilbey;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TestMod implements ModInitializer {
+	public static final String MOD_ID = "test-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
